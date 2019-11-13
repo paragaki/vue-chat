8d41bef33990602cc76da043a36585f942707ea2
paragaki <strangejourney28@gmail.com>
2019-11-13 09:09:08 +0900

    Subscribing to a Room

--- src/store/actions.js
+++ src/store/actions.js
@@ -18,6 +18,23 @@ export default {
         name: currentUser.name
       });
       commit('setReconnect', false);
+
+      // Save list of user's rooms in store
+      const rooms = currentUser.rooms.map(room => ({
+        id: room.id,
+        name: room.name
+      }));
+      commit('setRooms', rooms);
+
+      // Subscribe user to a room
+      const activeRoom = state.activeRoom || rooms[0]; // pick last used room, or the first one
+      commit('setActiveRoom', {
+        id: activeRoom.id,
+        name: activeRoom.name
+      });
+      await chatkit.subscribeToRoom(activeRoom.id);
+
+      return true;
     } catch (error) {
       handleError(commit, error)
     } finally {
